9235419b0fbbc0e5b82e692bf5d46f415f9dae4d
Christoffer Fransson <133678990+Christoffer-Fransson@users.noreply.github.com>
2026-07-30 14:41:03 +0200

    Add: Repoglobal shortcutstips file; And additions to day 1 code file

--- 100_days_of_python/day1/day1lessons.py
+++ 100_days_of_python/day1/day1lessons.py
@@ -1,5 +1,5 @@
 # Write your code below this line 👇
-# DAY 1: Lesson 1
+# DAY 1: Lesson 1 - print() 
 print("Hello world!")
 
 # DAY 1: Lesson 2
@@ -9,4 +9,29 @@ print("3. Add 3g of Salt.")
 print("4. Leave to rise for 2 hours.")
 print("5. Bake at 200 degrees C for 30 minutes.")
 
+
+# Debugging session
+# Fix the code below 👇
+'''
+print(Notes from Day 1")
+ print("The print statement is used to output strings")
+print("Strings are strings of characters"
+priint("String Concatenation is done with the + sign")
+print(("New lines can be created with a \ and the letter n")
+'''
+
+# Below code is fixed
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
+
+
+# DAY 1: Lesson 3 - input()
+# print("What is your name?")  	 # PRINTS ONLY
+# input("What is your name?)	  # Prints and lets user inputs text
+
+# Mini challenge
+print("Hello" + input("What is your name?") + "!")
 
---
+++ shortcuttips.md
@@ -0,0 +1,3 @@
+# Tips of useful shortcut commands (Windows)
+
+# WIN + /	COMMENTS or UNCOMMENTS the line your cursor are on
