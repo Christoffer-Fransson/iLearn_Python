41930a81b622a643916b15fb7ef3c7e35ebc210d
Christoffer Fransson <133678990+Christoffer-Fransson@users.noreply.github.com>
2026-07-31 09:36:32 +0200

    ADD: DAY2: Lesson - DataTypes -> .py file REMOVE: day2lessons.py from repo root [wrong placement]

--- 100_days_of_python/day2/day2lessons.py
+++ 100_days_of_python/day2/day2lessons.py
@@ -1,1 +1,26 @@
-#DAY2: LESSON 1 - 
+#DAY2: LESSON 1 - Datatypes
+	#1. STRINGS
+
+	# Subscripting = When you extract a positional character from a string
+	# print("Hello""[x]) # returns the character in string position where x defines which numbered position. first character position is 0
+	print("Hello"[4])
+
+	# TIP - SUBSCRIPTING SUPPORT NEGATIVE POSITION EG -2 Note; starts from position 0
+	print("Hello"[-4])
+
+	#String - numbers treated as strings not numbers
+	print("123" + "345") # returns 12345
+
+	#2.INTEGERS = WHOLE NUMBER
+	print (123+345) # Returns the numeric sum
+
+	# LARGE INTEGERS - Readability eg _can be used instead of 123,456,789 for large integer numbers.
+	print(123_456_789)
+
+	#3.FLOAT = Floating Point Number eg. Decimals  3141.59
+	print(3.14159)
+
+	#4.BOOLEAN - True/False note: capital T and F are important
+	print(True)
+	print(False)
+
