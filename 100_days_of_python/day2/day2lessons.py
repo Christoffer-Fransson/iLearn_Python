fbb719cb7d71bbecf75970d528acda13865c6073
Christoffer Fransson <133678990+Christoffer-Fransson@users.noreply.github.com>
2026-07-31 13:11:18 +0200

    EoD Update: Added lesson 5, challenge of the day.

--- 100_days_of_python/day2/day2lessons.py
+++ 100_days_of_python/day2/day2lessons.py
@@ -49,8 +49,6 @@
 	# All datatypes cannot be converted into other datatypes
 	# e.g. a string datatype lett eg "A" cannot be converted into an int
 
-
-
 	# Pause Challenge 3
 	# Make this line of code run without errors:
 	# print("Number of letters in your name: " + len(input("Enter your name")))
@@ -123,3 +121,76 @@
 	# How to change the result to 3
 
 #DAY2: LESSON 4 - Number Manipulation
+
+	# BMI Calculator challenge
+	height = 1.72
+	weight = 89
+
+	# Write your code here.
+	# Calculate the bmi using weight and height.
+	bmi = weight / (height ** 2)
+	print(bmi)
+	
+	# Flooring - removing decimals to the lower integer value -> using int()
+	print(int(10.12345))        # returns 10 as integer datatype
+	print(type(int(10.12345)))
+	print(bmi)                  # from the challenge above
+		
+	# ROUNDING
+	# using the round() function, rounds up or down to neareest integer value depending on the value of the first decimal
+	# round() takes two inputs: number you want to round; nr of digits you want to round it to
+	print(round(bmi))
+	print(round(3.499999))      # returns 3
+	print(round(3.5))           # returns 4
+	print(round(3.511111))      # returns 4
+	print(round(3.5114566,3))   # returns 3.511
+
+	print(round(bmi,2))         # returns the bmi float with two decimals of accuracy
+
+	# ASSIGNMENT OPERATOR   - Accumulates the result of our calculations
+	# Example
+	score = 0
+	score = score +1    # instead of writing this
+	score += 1          # You can write this to add a number of points
+	score -= 1          # or minus a number to subtract
+	score *= 5          # or multiply by number
+	score /= 2         # or divide by number
+
+	# F-Strings
+	# In python we can use an f-string to insert a variable or an expression into a string
+	# print(f"")   <--- note the f in front of the ""
+	# variables or expressions defined inside of {} -> print(F"{score}")
+	" {} are called curly braces"
+
+
+	print("Score: " + str(round(score)))    # Without f-string
+
+	score = 0
+	height = 1.8
+	is_winning = True
+	print(f"Your score = {score}. You are winning is: {is_winning}")        # With f-string
+
+#DAY2: LESSON 5 - Tip Calculator project
+
+	#DAY 2 CHALLENGE - my solution for calculating tip per person not total bill per person
+	print("Welcome to the tip calculator!")
+	bill = float(input("What was the total bill? $"))
+	tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+	people = int(input("How many people to split the bill? "))
+	tip_per_person = ((bill * (1 + (tip/100))-bill) / people)
+	print(f"Each person should pay tip: {round(tip_per_person,2)}")
+
+	# Teachers solution
+	print("Welcome to the tip calculator!")
+	bill = float(input("What was the total bill? $"))
+	tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+	people = int(input("How many people to split the bill? "))
+	tip_as_percent = tip / 100
+	total_tip_amount = bill * tip_as_percent
+	total_bill = bill + total_tip_amount
+	bill_per_person = total_bill / people
+	final_amount = round(bill_per_person, 2)
+	print(f"Each person should pay: ${final_amount}")
+
+
+#---  End of day 2 ---
