6ae6685ce147d1956877037b061c305169139150
Christoffer Fransson <133678990+Christoffer-Fransson@users.noreply.github.com>
2026-08-02 15:40:42 +0200

    UPDATE; day3lessons.py updated with lessen five code challenge

--- 100_days_of_python/day3/day3lessons.py
+++ 100_days_of_python/day3/day3lessons.py
@@ -202,3 +202,87 @@ elif bmi >= 18.5 < 25:
 elif bmi >= 25:
     print("overweight")
 #DAY3: Lesson 4 - Multiple ifs
+'''
+
+                 .---------.
+                /    age    \
+                \           /
+                 '---------'
+     under 12   /     |     \   Over 18
+               v       v      v
+        +--------+ +--------+ +--------+
+        |   $5   | |   $7   | |  $12   |
+        +--------+ +--------+ +--------+
+                      12-18
+             \        |        /
+              \       |       /
+               v      v      v
+                (merge point)
+                      |
+                      v
+                 .-----------.
+                /    want     \
+                \   photos?   /
+                 '-----------'
+             No /             \ Yes
+               v               v
+        (straight down)   +--------+
+               |           |   $3   |
+               |           +--------+
+                \             /
+                 \           /
+                  v         v
+            +---------------------+
+            | The total bill is $x|
+            +---------------------+
+'''
+
+# 	Roller coast example with multiple ifs
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age? "))
+    if age <= 12:
+        bill = 5
+        print("Child tickets are $5.")
+    elif age <= 18:
+        bill = 7
+        print("Youth tickets are $7.")
+    else:
+        bill = 12
+        print("Adult tickets are $12.")
+
+    wants_photo = input("Do you want to have a photo taken? Type y for yes and n for No")
+    if wants_photo == "y":
+        # Add $3 to their bill
+        bill += 3
+    print(f"Your final bill is ${bill}")
+else:
+    print("Sorry you have to grow taller before you can ride.")
+
+#DAY3: LESSON 5 - Coding challenge
+#	My solution
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M or L: ")
+pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+bill = 0
+
+if size == "S":
+    bill +=15
+elif size =="M":
+    bill +=20
+elif size =="L":
+    bill +=25
+if pepperoni == "Y":
+   if size == "S":
+       bill +=2
+   else:
+       bill +=3
+if extra_cheese == "Y":
+    bill +=1
+print(f"Your final bill is: ${bill}.")
