23fc62c9d2c8a5965de3856021dc6a0a27d97dbd
Christoffer Fransson <133678990+Christoffer-Fransson@users.noreply.github.com>
2026-08-02 12:52:11 +0200

    UPDATE: Lesson 1 day 3 added to day3lesson.py

--- 100_days_of_python/day3/day3lessons.py
+++ 100_days_of_python/day3/day3lessons.py
@@ -1,2 +1,56 @@
 #DAY3: Control Flow and Logical Operators
+
 #DAY3: LESSON 1 - IF ELSE CONDITIONS
+'''	 Example: IF statement
+	water_level = 50
+	if water_level >  80:
+		print("Drain water")
+	else: 
+		print("Continue")
+
+
+
+
+-->             An if statement can be visualised by a XOR gate. 
+                Cross pollination with process development / charting
+                
+                +-----------+
+                |   Start   |
+                +-----------+
+                      |
+                      v
+                 .-----------.
+                /  Height >   \
+                \   120cm?    /
+                 '-----------'
+             No /             \ Yes
+               v               v
+      +---------------+  +-------------+
+      |   Can't ride   |  |  Can ride   |
+      +---------------+  +-------------+
+'''
+
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+else:
+    print("Sorry you have to grow taller before youc an ride.")
+
+   # COMPARISON OPERATORS
+'''
+    Operator    Meaning
+    >           Greater than
+    >           Less than
+    >=          greather than or equal to
+    <=          less than or equal to
+    ==          equal to
+    !=          not equal to
+'''
+
+#DAY3: LESSON 2 - Modulo operator %
+
+
