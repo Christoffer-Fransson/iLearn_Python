daa8f7dea90eeb0d771d24f18ff5a64e8f3647d8
Christoffer Fransson <133678990+Christoffer-Fransson@users.noreply.github.com>
2026-08-08 18:32:31 +0200

    Update: day7/day7lessons.py - Updated with hangman lesson 2, iterating string checking

--- 100_days_of_python/day7/day7lessons.py
+++ 100_days_of_python/day7/day7lessons.py
@@ -16,3 +16,34 @@ else:
     print("Wrong")
 
 
+#   Step 2 - Next todo steps
+'''
+TODO-1
+Create an empty String called placeholder.
+For each letter in the chosen_word, add a _ to placeholder.
+So if the chosen_word was "apple", placeholder should be _ _ _ _ _ with 5 "_" representing each letter to guess.
+Print out hint.
+'''
+placeholder = str()
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print(placeholder)
+'''
+TODO-2
+Create an empty string called "display".
+Loop through each letter in the chosen_word
+If the letter at that position matches guess then reveal that letter in the display at that position.
+e.g. If the user guessed "p" and the chosen word was "apple", then display should be _ p p _ _.
+Print display and you should see the guessed letter in the correct position.
+But every letter that is not a match is represented with a "_".
+'''
+display = str()
+for letter in chosen_word:
+    if letter == guess:
+        display += guess
+    else:
+        display += "_"
+print(display)
+
+
