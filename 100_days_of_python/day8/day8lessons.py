f8939b61471fad9fd25abfc2a50f6cdc1423a45c
Christoffer Fransson <133678990+Christoffer-Fransson@users.noreply.github.com>
2026-08-14 13:05:59 +0200

    Update: day8/day8lessons.py - First ceasar cipher lesson solution (my own) done

--- 100_days_of_python/day8/day8lessons.py
+++ 100_days_of_python/day8/day8lessons.py
@@ -77,5 +77,33 @@ def calculate_love_score(name_a, name_b):
 
 calculate_love_score("Angela Yu", "Jack Bauer")    
 
-#	LESSON 6 - CEASER CYUPER - Part 1 Encryption
+#	LESSON 6 - CEASER CYPHER- Part 1 Encryption
 
+from quopri import encodestring
+
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#	My solition to first step of Caesar cpher
+
+# TODO-1: Create a function called 'encrypt()' that takes 'original_text' and 'shift_amount' as 2 inputs.
+def encrypt(original_text, shift_amount):
+    encoded_text = ""
+
+# TODO-2: Inside the 'encrypt()' function, shift each letter of the 'original_text' forwards in the alphabet
+#  by the shift amount and print the encrypted text.
+    encrypted_char_index = []
+    for character in text:
+        real_char_index = alphabet.index(character)
+        encrypted_char_pos = int(real_char_index) + shift_amount
+        encoded_text = encoded_text + alphabet[encrypted_char_pos]
+    print(encoded_text)
+
+# TODO-4: What happens if you try to shift z forwards by 9? Can you fix the code?
+#   returns qnuux
+# TODO-3: Call the 'encrypt()' function and pass in the user inputs. You should be able to test the code and encrypt a
+#  message.
+encrypt(text, shift)
