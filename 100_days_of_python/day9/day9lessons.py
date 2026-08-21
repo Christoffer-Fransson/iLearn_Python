0a84ed281e4d6159f8463025d83f2640aa8cd165
Christoffer Fransson <133678990+Christoffer-Fransson@users.noreply.github.com>
2026-08-21 09:27:34 +0200

    Update: /day9/day9lessons.py with my refined solution and teachers solution for the hidden bid auction

--- 100_days_of_python/day9/day9lessons.py
+++ 100_days_of_python/day9/day9lessons.py
@@ -94,6 +94,8 @@ print(travel_log2["Germany"]["cities_visited"][2])      # Teachers solution
 
 
 #DAY9: Blind Auction Project
+# --- My Solution ---
+
 # TODO-1: Ask the user for input
 # TODO-2: Save data into dictionary {name: price}
 # TODO-3: Whether if new bids need to be added
@@ -120,11 +122,44 @@ while keep_running == True:
         keep_running = False
     else:
         keep_running = True
-# TODO-4: Compare bids in dictionary     
+# TODO-4: Compare bids in dictionary
 winning_bid = max(received_bids["Bids"])
-# for bid in received_bids["Bids"]:
+winner = received_bids["Bidder"][received_bids["Bids"].index(winning_bid)]
+
+# for bid in received_bids["Bids"]:             # Max() works similar to this. this is a solution if a mechanical examination is required
 #     if bid > winning_bid:
 #         winning_bid = bid
-winning_bid_position = received_bids["Bids"].index(winning_bid)
-print(f'{received_bids["Bidder"][winning_bid_position]} bidding {received_bids["Bids"][winning_bid_position]} won the auction')
+
+print(f'The winner is {winner} with a bid of ${winning_bid}')
+
+# --- Teachers solution ---
+
+from art import logo
+print(logo)
+
+
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with a bid of ${highest_bid}")
+
+
+bids = {}
+continue_bidding = True
+while continue_bidding:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+    if should_continue == "no":
+        continue_bidding = False
+        find_highest_bidder(bids)
+    elif should_continue == "yes":
+        print("\n" * 20)
+
 
