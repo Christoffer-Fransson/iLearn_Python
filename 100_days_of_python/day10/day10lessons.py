2ab197232cc2cb61151e91c01963e7b7fac9e79a
Christoffer Fransson <133678990+Christoffer-Fransson@users.noreply.github.com>
2026-08-21 12:39:41 +0200

    Update: day10/day10lessons.py - with Lesson 2 - Multiple return values

--- 100_days_of_python/day10/day10lessons.py
+++ 100_days_of_python/day10/day10lessons.py
@@ -1,4 +1,4 @@
-# DAY10: Functions with Outputs - Lesson 1
+# DAY10: Lesson 1 -  Functions with Outputs
 def format_name(f_name, l_name):
     formated_f_name = f_name.title()
     formated_l_name = l_name.title()
@@ -18,3 +18,16 @@ def function_2(text):
 
 output = function_2(function_1("Hello"))
 print(output)
+
+
+# DAY10: Lesson 2 - Multiple Return Values
+def format_name(f_name, l_name):
+    if f_name == "" or l_name == "":
+        return "You did not provide valid inputs"
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+    print("This got printed")   # will not be printed because it is not included in the return
+
+print(format_name(input("What is your first name? "), input("what is your last name? ")))
+
