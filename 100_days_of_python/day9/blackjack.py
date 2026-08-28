3b0910b86bf0c856b8116263c814cac265779756
Christoffer Fransson <133678990+Christoffer-Fransson@users.noreply.github.com>
2026-08-28 14:43:58 +0200

    Refactor: blackjack.py - Redid function for dealing hands to accomodate both computer and player and callable with amount of cards

--- 100_days_of_python/day9/blackjack.py
+++ 100_days_of_python/day9/blackjack.py
@@ -2,13 +2,27 @@
 import art
 import random
 card = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-dealer_hand = []
+computer_hand = []
 player_hand = []
 print(art.logo)
 
 def draw_card():
     card_value = random.choice(card)
     return card_value
+
+def deal_hand(amount_cards, hand_type):
+    i = 0
+    while i < amount_cards:
+        if hand_type == "player":
+            player_hand.append(draw_card())
+            print("do stuff")
+        if hand_type == "computer":
+            computer_hand.append(draw_card())
+            print("do_stuff")
+        i += 1
+    return player_hand, computer_hand
+
+# ---------
 
 def check_bust(player_hand, dealer_hand):
     if sum(player_hand) > 21:
@@ -18,6 +32,8 @@ def check_bust(player_hand, dealer_hand):
 
 bust = False
 while not bust:
+    print(art.logo)
+
     while len(player_hand) <2 and len(dealer_hand) <2:
         print("dealer deals card to player")
         player_hand.append(draw_card())
@@ -34,6 +50,8 @@ while not bust:
         print(f'Player hand: {player_hand}')
         print(f"player hand sum = {sum(player_hand)}")
 
+    if choice.lower() == "stay":
+        print("Player stays")
     if choice.lower() == "q":
         bust = True
 
