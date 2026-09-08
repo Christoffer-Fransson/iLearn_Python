15f9729b75ae7145fb21791ab619c0c25f5a8503
Christoffer Fransson <133678990+Christoffer-Fransson@users.noreply.github.com>
2026-09-08 18:13:29 +0200

    Minor Refactor - blackjack.py check_bust() -> check_hand() for future expanded functionality

--- 100_days_of_python/day11/blackjack.py
+++ 100_days_of_python/day11/blackjack.py
@@ -1,5 +1,4 @@
 #DAY11: Blackjack capstone project
-from operator import truediv
 
 import art
 import random
@@ -19,7 +18,7 @@ def deal_hand(amount_cards, hand_type):
         i += 1
     return player_hand, computer_hand
 
-def check_bust(hand, hand_type):
+def check_hand(hand, hand_type):
     if sum(hand) < 21:
         if hand_type.lower() == "player":
             print(f'    Your cards: {player_hand}, current score: {sum(player_hand)}')
@@ -31,16 +30,16 @@ def check_bust(hand, hand_type):
     else:           # if >21 returns false killing the loop
         return False
 
-run = True
 
 # Game start / Outer game loop
+run = True
 while run:
     computer_hand = []          # Sets/resets player hand to empty
     player_hand = []            # Sets/resets computer hand to empty
     run_choice = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
 
-    # Inner game interactivity loop
     if run_choice.lower() == "y":
+        # Inner game interactivity loop
         print(art.logo)
         deal_hand(2,"player")
         deal_hand(2,"computer")
@@ -52,7 +51,7 @@ while run:
             choice = input("Type 'y' to get another card, type 'n' to pass  ")
             if choice.lower() == "y":
                 deal_hand(1,"player")
-                keep_playing = check_bust(player_hand, hand_type="player")              # checks hand sum and if stop loop criteria is met
+                keep_playing = check_hand(player_hand, hand_type="player")              # checks hand sum and if stop loop criteria is met
                 print(f"Your Cards: {player_hand}, current score {sum(player_hand)}")
 
             if choice.lower() == "n":
@@ -67,6 +66,7 @@ while run:
  #       print(f'Your Cards: {player_hand}, current score: {sum(player_hand)}')
  #       print(f"Computer's first card: {computer_hand[0]}")
 
+    #Outer game loop - for when the user wants to quit playing BlackJack
     if run_choice.lower() == "n":
         run = False
 
