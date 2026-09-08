5433d80624751c516ecce27faa6376446d8dc2cb
Christoffer Fransson <133678990+Christoffer-Fransson@users.noreply.github.com>
2026-09-08 17:53:44 +0200

    Fix: blacjack.py - Fixed outro text placement according to requirements

--- 100_days_of_python/day11/blackjack.py
+++ 100_days_of_python/day11/blackjack.py
@@ -5,7 +5,6 @@ import art
 import random
 card = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-
 def draw_card():
     card_value = random.choice(card)
     return card_value
@@ -29,17 +28,18 @@ def check_bust(hand, hand_type):
         return True
     elif sum(hand) == 21:
         print("temp yay")
-    else:
-        print(f"Your final hand: {player_hand}, final score: {sum(player_hand)}")
-        print(f"Computers final hand: {sum(computer_hand)}")
-        print("You went over. You loose 😢")
+    else:           # if >21 returns false killing the loop
         return False
 
 run = True
+
+# Game start / Outer game loop
 while run:
     computer_hand = []          # Sets/resets player hand to empty
     player_hand = []            # Sets/resets computer hand to empty
     run_choice = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
+
+    # Inner game interactivity loop
     if run_choice.lower() == "y":
         print(art.logo)
         deal_hand(2,"player")
@@ -54,11 +54,18 @@ while run:
                 deal_hand(1,"player")
                 keep_playing = check_bust(player_hand, hand_type="player")              # checks hand sum and if stop loop criteria is met
                 print(f"Your Cards: {player_hand}, current score {sum(player_hand)}")
+
             if choice.lower() == "n":
                 print("temp:stands")
 
-        print(f'Your Cards: {player_hand}, current score: {sum(player_hand)}')
-        print(f"Computer's first card: {computer_hand[0]}")
+    print(f"Your final hand: {player_hand}, final score: {sum(player_hand)}")
+    print(f"Computers final hand: {computer_hand}, final score:{sum(computer_hand)}")
+    if sum(player_hand) > 21:
+        print("You went over. You loose 😢")
+
+
+ #       print(f'Your Cards: {player_hand}, current score: {sum(player_hand)}')
+ #       print(f"Computer's first card: {computer_hand[0]}")
 
     if run_choice.lower() == "n":
         run = False
